5d52d99bb9e1d90066028d4b3b967f8178dd412a
ericwyn <ericwyn.chen@gmail.com>
2023-04-03 20:06:52 +0800

    support -v params

--- scann.go
+++ scann.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"scann/zbar"
 )
 
+var verFlag = flag.Bool("v", false, "show version")
+var versionStr = "1.0.0, 2023-04-03"
+
 func main() {
+	flag.Parse()
+
+	if *verFlag {
+		fmt.Println(versionStr)
+		return
+	}
+
 	zbar.SmartScan()
 }
