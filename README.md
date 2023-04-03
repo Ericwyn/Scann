665541a2c4fd56ba2860f6b7ce3f0910b84fff71
ericwyn <ericwyn.chen@gmail.com>
2023-04-03 20:04:54 +0800

    remove app tray, update README.md

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
-# SCANN
+# Scann
 
 一个 linux 上面快速截图和识图工具
 
-支持识别以下图形
+启动之后可进行截图，并自动识别二维码或条形码
+
+识别结果自动写入剪贴板
+
+目前版本支持识别以下图形
 
 - qrcode 二维码
 - barcode 条形码
 
 todo
-- ocr 快速识别文字
+- ocr ?
+- 智能搜索 ？
+- 二维码、条形码生成 ？
+
+可搭配 [fastray](https://github.com/Ericwyn/fastray) 项目来实现 appTray 处快捷启动
+
+
+![](doc/screenshot.png)
 
 ## 项目依赖
 项目在 ubuntu 20.04 下开发和测试，依赖以下命令
--- scann.go
+++ scann.go
@@ -1,26 +1,9 @@
 package main
 
 import (
-	"flag"
-	"scann/log"
-	"scann/ui"
 	"scann/zbar"
 )
 
-// 支持 -bar 和 -qr 两个参数，分别表示条形码和二维码
-var barCode = flag.Bool("bar", false, "bar code scan")
-var qrCode = flag.Bool("qr", false, "qr code scan")
-
-var uiMode = flag.Bool("ui", false, "ui mode")
-
 func main() {
-	flag.Parse()
-
-	if *uiMode {
-		log.I("run in ui mode")
-		ui.ShowAppTray()
-		return
-	} else {
-		zbar.SmartScan()
-	}
+	zbar.SmartScan()
 }
